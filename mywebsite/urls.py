c0424c50ea26afb210340001db14eaa32b558c4d
BingGo <binggo@BingGos-MacBook-Pro.local>
2021-07-24 13:46:46 +0700

    08 template pada App

---
+++ mywebsite/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url(r'^blog/',include('blog.urls')),
+]
